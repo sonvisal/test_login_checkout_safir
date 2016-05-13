b89a624dbe78639d153bc8f578691bf225fdce32
sonvisal <sonvisal10@gmail.com>
2016-05-13 17:24:22 +0700

    add more grep

--- run_testing.sh
+++ run_testing.sh
@@ -3,5 +3,5 @@ filename=$(date +'%Y-%m-%d-%T')-error.log
 echo $filename
 export  ROOT_URL=http://localhost:3000
 
-chimp --ddp=http://localhost:3000/ --watch | grep -e "still not existing after 50000ms|Error: An element could not be located" > $filename
+chimp --ddp=http://localhost:3000/ --watch | grep -e "Error|RuntimeError|still not existing after 50000ms|Error: An element could not be located" > $filename
 #chimp --watch --ddp=$ROOT_URL --path=test_login_checkout_safir
