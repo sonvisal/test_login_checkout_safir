a50e53960ac4e3da86c6bf477a44eeac1e1015f0
sonvisal <sonvisal10@gmail.com>
2016-05-13 14:27:02 +0700

    change run

--- run_testing.sh
+++ run_testing.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-chimp --ddp=http://localhost:3000 --watch | egrep -w "still not existing after 50000ms | Error: An element could not be located" -A 5 -B 5 > error_image.log
+chimp --ddp=http://localhost:3000 --watch | grep -w "still not existing after 50000ms\| Error: An element could not be located" -A 5 -B 5 > error_image.log
