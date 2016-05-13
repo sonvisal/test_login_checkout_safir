0534eb8a41ae2e6a087f5abd038b7f82916e2b66
sonvisal <sonvisal10@gmail.com>
2016-05-13 14:58:15 +0700

    create file log by date

--- run_testing.sh
+++ run_testing.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-chimp --ddp=http://localhost:3000 --watch | grep -w "still not existing after 50000ms\| Error: An element could not be located" -A 5 -B 5 > error_image.log
+filename=$(date +'%Y-%m-%d')-error.log
+echo $filename
+chimp --ddp=http://localhost:3000 --watch | grep -e "still not existing after 50000ms|Error: An element could not be located" -A 5 -B 5 > $filename
