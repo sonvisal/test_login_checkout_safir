c164a85d85f20c5e7797afa4e1c8927e124dd639
sonvisal <sonvisal10@gmail.com>
2016-05-13 17:47:30 +0700

    change error file

--- run_testing.sh
+++ run_testing.sh
@@ -3,5 +3,6 @@ filename=$(date +'%Y-%m-%d-%T')-error.log
 echo $filename
 export  ROOT_URL=http://localhost:3000
 
-chimp --ddp=http://localhost:3000/ --watch | grep -e "Error|RuntimeError|still not existing after 50000ms|Error: An element could not be located" -A 5 -B 5 >  $filename
+chimp --ddp=http://localhost:3000/ --watch >$filename
 #chimp --watch --ddp=$ROOT_URL --path=test_login_checkout_safir
+cat $filename | grep -e "Error|RuntimeError|still not existing after 50000ms|Error: An element could not be located" -A 5 -B 5 > error.log
