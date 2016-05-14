93ff3bf58165f2f73749594e11c249f69459bd57
sonvisal <sonvisal10@gmail.com>
2016-05-14 11:45:34 +0700

    delete | tr -d [32m

--- run_testing.sh
+++ run_testing.sh
@@ -3,8 +3,10 @@ filename=$(date +'%Y-%m-%d-%T')-error.log
 echo $filename
 export  ROOT_URL=http://localhost:3000
 
-chimp --ddp=http://localhost:3000/ --watch >$filename
+chimp --ddp=http://localhost:3000/ --watch | tr -d "[32m||[90m|[33m|[35m|39m" | grep -e "Error" -A 5 -B 2  >& $filename
+
+# | tr -d "" | tr -d "[90m"|
 #chimp --watch --ddp=$ROOT_URL --path=test_login_checkout_safir
-echo "last file"
-echo $filename
-cat  $filename | grep -e "Error" -A 5 -B 2 > error.log
+# echo "last file"
+# echo $filename
+# cat  $filename | grep -e "Error" -A 5 -B 2 > error.log
